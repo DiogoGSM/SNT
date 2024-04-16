579c97665c4ea661191247a584571fc33a16978a
DiogoGSM <diogo.g.s.marques@gmail.com>
2024-04-16 16:24:32 +0100

    Tests with Bokeh server to python callbacks

---
+++ webapp/bokehtests/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, Slider, TextInput
+from bokeh.plotting import figure
